cd6f6c1aa64410bfe75fb720f2811670df7a0efb
Jonathan <jberry4@bearcubs.santarosa.edu>
2026-06-06 15:08:17 -0700

    refactor: render hero using JavaScript

--- src/home.js
+++ src/home.js
@@ -1,2 +1,19 @@
 import "./styles.css";
 import "./home.css";
+
+renderHome();
+
+function renderHome(list) {
+  renderHero();
+}
+
+function renderHero() {
+  const container = document.createElement("div");
+  container.className = "hero";
+  document.body.appendChild(container);
+
+  const heading = document.createElement("h1");
+  heading.innerText = "Your Daylists";
+  heading.className = "hero-text";
+  container.appendChild(heading);
+}
