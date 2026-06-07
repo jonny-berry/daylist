ac47da7c9da764cbd7d2aecd6e34f5822ab10d75
Jonathan <jberry4@bearcubs.santarosa.edu>
2026-06-06 19:07:23 -0700

    refactor: render create new list section with JavaScript

--- src/home.js
+++ src/home.js
@@ -1,11 +1,26 @@
 import "./styles.css";
 import "./home.css";
+import whitePlusIcon from "./assets/icons/add-white.svg";
+import horizontalDots from "./assets/icons/more.svg"
+import { testList } from "./test-data.js";
 
-renderHome();
 
-function renderHome(list) {
+
+renderHomePage();
+
+
+
+function renderHomePage(list) {
   renderHero();
+  
+  const listsDisplay = document.createElement("div");
+  listsDisplay.className = "lists-display";
+  document.body.appendChild(listsDisplay);
+
+  renderCreateList(listsDisplay);
 }
+
+
 
 function renderHero() {
   const container = document.createElement("div");
@@ -16,4 +31,36 @@ function renderHero() {
   heading.innerText = "Your Daylists";
   heading.className = "hero-text";
   container.appendChild(heading);
+}
+
+
+
+function renderCreateList(listsDisplay) {
+  const newListContainer = document.createElement("div");
+  newListContainer.className = "new-list-container";
+  listsDisplay.appendChild(newListContainer);
+
+  const sidebar = document.createElement("div");
+  sidebar.className = "list-sidebar";
+  newListContainer.appendChild(sidebar);
+
+  const sidebarBtn = document.createElement("button");
+  sidebarBtn.className = "sidebar-btn";
+  sidebar.appendChild(sidebarBtn);
+
+  const plugImage = document.createElement("img");
+  plugImage.src = whitePlusIcon;
+  sidebarBtn.appendChild(plugImage);
+
+  const heading = document.createElement("h2");
+  heading.textContent = "Create new Daylist";
+  newListContainer.appendChild(heading);
+
+  const moreOptionsBtn = document.createElement("button");
+  moreOptionsBtn.className = "list-options-btn";
+  newListContainer.appendChild(moreOptionsBtn);
+
+  const moreOptionsImage = document.createElement("img");
+  moreOptionsImage.src = horizontalDots;
+  moreOptionsBtn.appendChild(moreOptionsImage);
 }
