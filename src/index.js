d41633f2c3f916afabf9b19b91574bc0ea931785
Jonathan <jberry4@bearcubs.santarosa.edu>
2026-06-10 16:20:12 -0700

    chore: remove unused 'open as' buttons from list dropdown

--- src/index.js
+++ src/index.js
@@ -223,16 +223,6 @@ function renderListDropdown(list, imageElParent) {
     saveState();
   })
 
-  const openAsViewerBtn = document.createElement("button");
-  openAsViewerBtn.innerText = "Open as viewer";
-  openAsViewerBtn.classList = "option bordered-option";
-  container.appendChild(openAsViewerBtn);
-
-  const openAsEditorBtn = document.createElement("button");
-  openAsEditorBtn.innerText = "Open as editor";
-  openAsEditorBtn.className = "option";
-  container.appendChild(openAsEditorBtn);
-
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Delete list";
   deleteBtn.className = "option";
